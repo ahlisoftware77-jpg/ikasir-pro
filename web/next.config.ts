ea04dc5cf7927cbab05e8f7eb7b722e139d3a41e
Yadi <00563@china-glaze0.co.id>
2026-06-07 11:57:46 +0700

    Add Firebase Auth reverse proxy for custom domain

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   allowedDevOrigins: ["localhost:3000", "192.168.15.25:3000", "*.loca.lt"],
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination: 'https://kasir-3d12b.firebaseapp.com/__/auth/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
